204813d08f7f06e1b891cdeb3da34f880cf9177a
chesterchenn <chesterchenn@gmail.com>
2021-05-16 23:57:03 +0800

    fix: 修复在不同目录下启动nohup的问题

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-rm ./nohup.out
-nohup jekyll serve &
+
+# 获取当前路径
+path=$(cd `dirname "$0"`;pwd)
+
+# 移除 nohup.out 文件
+$(rm $path/nohup.out)
+
+# 启动服务
+$(nohup jekyll serve --config $path/_config.yml -s $path/ -d $path/_site > $path/nohup.out &)
