4555f0f5f374defe32f305617da015691ca34b3c
chesterchenn <chesterchenn@outlook.com>
2021-10-12 11:00:13 +0800

    build: 移除安装位置

--- script/init-deb.zsh
+++ script/init-deb.zsh
@@ -4,18 +4,8 @@
 # 安装 ruby
 sudo apt install ruby-full build-essential zlib1g-dev
 
-# 指定 gem 安装目录
-count=$(grep -c 'export GEM_HOME="$HOME/gems"' ~/.zshrc)
-if [ $count -eq 0 ]; then
-  echo '# Install Ruby Gems to ~/gems'
-  echo 'export GEM_HOME="$HOME/gems"' >> ~/.zshrc
-  echo 'export PATH=$HOME/gems/bin:$PATH' >> ~/.zshrc
-  echo '\n' >> ~/.zshrc
-  source ~/.zshrc
-fi
-
 # 安装 jekyll bundler
-gem install jekyll bundler:2.2.22
+gem install jekyll:2.4.0 bundler:2.2.22
 
 # 安装依赖
 bundle install
